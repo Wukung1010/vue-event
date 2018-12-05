2727ec5646cbe5b1b88395488701afdf8861aa8a
sunxianfeng <sunxianfeng@jiuqi.com.cn>
2018-12-05 17:28:39 +0800

    重构了一些写法

--- src/pureFn.js
+++ src/pureFn.js
@@ -1,6 +1,6 @@
 function _on(listeners, type, fn) {
-    let arr = listeners[type];
-    if (!arr) {
+    let arr = null;
+    if (arr = listeners[type], !arr) {
         arr = [];
         listeners[type] = arr;
     }
@@ -10,23 +10,21 @@ function _on(listeners, type, fn) {
 function _off(listeners, type, fn) {
     let listenerArr = null;
     let index = null;
-    if(listenerArr = listeners[type], index = listenerArr.indexOf(fn), index > -1) {
+    if(listenerArr = listeners[type], listenerArr && (index = listenerArr.indexOf(fn)), index > -1) {
         listenerArr.splice(index, 1);
     }
 }
 
 function _errorHandler(glabal, listeners, e, type, ...args) {
-    if(glabal.length > 0) {
-        glabal.forEach(listener => {
-            try{
-                listener(e, type, ...args);
-            }catch(e) {
-                console.error(`全局错误处理器 ${funcName(listener)} 发生错误：${e}`);
-            }
-        });
-    }
-    let arr = listeners[type];
-    if(arr && arr.length > 0) {
+    glabal.forEach(listener => {
+        try{
+            listener(e, type, ...args);
+        }catch(e) {
+            console.error(`全局错误处理器 ${funcName(listener)} 发生错误：${e}`);
+        }
+    });
+    let arr = null;
+    if(arr = listeners[type], arr) {
         arr.forEach(listener => {
             try{
                 listener(e, type, ...args);
